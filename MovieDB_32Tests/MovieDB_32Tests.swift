c672e505e4c852e5120a12bfd3264dceda6623b0
le.vu.tan.tuan <le.vu.tan.tuan@b121117-mb.local>
2018-12-18 14:16:51 +0700

    Init Project

---
+++ MovieDB_32Tests/MovieDB_32Tests.swift
@@ -0,0 +1,33 @@
+//
+//  MovieDB_32Tests.swift
+//  MovieDB_32Tests
+//
+//  Created by le.vu.tan.tuan on 12/18/18.
+//  Copyright © 2018 le.vu.tan.tuan. All rights reserved.
+//
+
+import XCTest
+@testable import MovieDB_32
+
+class MovieDB_32Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
